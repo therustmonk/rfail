6fc0396bcb01947105752e39bf64639f3daaf82f
Denis Kolodin <deniskolodin@gmail.com>
2017-03-26 11:19:03 +0300

    Improve logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate log;
+extern crate env_logger;
 
 mod broker;
 
@@ -9,29 +10,38 @@ use std::sync::mpsc::{channel, Sender};
 use broker::{SendBroker, Registrar, Finder};
 
 struct Call {
-    value: u8,
+    value: u16,
     listener: Sender<Answer>,
 }
 
 struct Answer {
-    value: u8,
+    value: u16,
 }
 
+// Counters
+const GLOBAL: u16 = 1000;
+const LOCAL: u16 = 3;
+const REG_ID: u16 = 1;
+
 fn main() {
-    let broker: SendBroker<u8, Call> = SendBroker::new();
-    for a in 1..10000 {
+    env_logger::init().unwrap();
+    let broker: SendBroker<u16, Call> = SendBroker::new();
+    for a in 0..GLOBAL {
         let finder = broker.clone();
         let rbroker = broker.clone();
+        debug!("- - - - - - - - - - - - - - - - - - - -");
+        debug!(">>>>> {} >>>>>", a);
         let shandler = thread::spawn(move || {
             let registrar = rbroker.clone();
             let rhandler = thread::spawn(move || {
                 let (tx, rx) = channel();
-                registrar.reg_sender(1, tx).expect("register to broker");
+                registrar.reg_sender(REG_ID, tx).expect("register to broker");
                 let mut optrx = Some(rx);
-                for x in 1..3 {
+                for x in 0..LOCAL {
                     let rx = optrx.take().expect("rx unwrap");
                     match rx.recv_timeout(Duration::from_millis(1)) {
                         Ok(Call { listener, value }) => {
+                            debug!("{} - request -> {}", a, value);
                             assert_eq!(value, x);
                             listener.send(Answer { value }).expect("sending response");
                         },
@@ -40,14 +50,16 @@ fn main() {
                     }
                     optrx = Some(rx);
                 }
-                registrar.unreg_sender(1).expect("remove from broker");
+                registrar.unreg_sender(REG_ID).expect("remove from broker");
             });
-            for x in 1..3 {
+            for x in 0..LOCAL {
                 loop {
-                    if let Some(service) = finder.find_sender(1).expect("finding sender") {
+                    if let Some(service) = finder.find_sender(REG_ID).expect("finding sender") {
+                        debug!("{} - found : {}", a, x);
                         let (tx, rx) = channel();
                         service.send(Call { listener: tx, value: x }).expect("sending request");
                         let Answer { value } = rx.recv().expect("receive result");
+                        debug!("{} - response <- {}", a, value);
                         assert_eq!(value, x);
                         break;
                     }
@@ -56,7 +68,8 @@ fn main() {
             rhandler.join().unwrap();
         });
         shandler.join().unwrap();
+        debug!("<<<<< {} <<<<<", a);
         trace!("Iter: {}", a);
-        println!("Done!");
     }
+    println!("Done!");
 }
